088fc135be6a9e09ce74fa5d985219c8c8cada2a
Sebastian <schwarz.duscheleit@hotmail.de>
2022-12-07 16:24:37 +0100

    update

--- process_sql_query.py
+++ process_sql_query.py
@@ -3,49 +3,47 @@ import mysql.connector
 import numpy as np
 
 
-def create(sql_query):
-
-    cnx = mysql.connector.connect(user='root', password='Dhwoiue832749823',
-                              host='127.0.0.1',
-                              database='Jazz')
-
+def create(sql_query, cnx):
 
     cursor = cnx.cursor()
 
-    query = (sql_query)
+ 
 
-    cursor.execute(query)
+    cursor.execute(sql_query)
 
     rows  = cursor.fetchall()
 
     list_of_lists = [list(elem) for elem in rows]
 
 
-  # geth columns names
+    # get columns names
 
     headings = [i[0] for i in cursor.description]
 
 
 
     layout = [
-            [sg.Table(values=list_of_lists, 
-            headings=headings, 
-            max_col_width=35,
-                        auto_size_columns=True,
-                        display_row_numbers=True,
-                        justification='right',
-                        num_rows=10,
-                        key='-TABLE-',
-                        row_height=35)]
-        ]
+                [sg.Table(values=list_of_lists, 
+                headings=headings, 
+                max_col_width=35,
+                            auto_size_columns=True,
+                            display_row_numbers=True,
+                            justification='right',
+                            num_rows=10,
+                            key='-TABLE-',
+                            row_height=35)]
+            ]
 
-    window = sg.Window("Contact Information Window", layout)
+    window = sg.Window(sql_query, layout)
 
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
-            
+
     window.close()
-    cursor.close()
-    cnx.close()
+    
+
+
+
+    
